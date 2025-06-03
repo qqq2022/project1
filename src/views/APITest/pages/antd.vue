be1242d5e5c8e4af9209cdb554baca402dca73dc
qqq2022 <410747672@qq.com>
2025-06-03 19:11:55 +0800

    11111

--- src/views/APITest/pages/antd.vue
+++ src/views/APITest/pages/antd.vue
@@ -1,105 +1,108 @@
 <template>
-  <a-layout style="min-height: 100vh">
-    <!-- 固定定位的侧边栏 -->
-    <a-layout-sider
-      v-model:collapsed="collapsed"
-      collapsible
-      :trigger="null"
-      :width="200"
-      :style="{
-        position: 'fixed',
-        height: '100vh',
-        left: 0,
-        top: 0,
-        zIndex: 100,
-      }"
-      class="custom-sider"
-    >
-      <div class="logo">LOGO</div>
-      <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys">
-        <a-menu-item key="1">
-          <user-outlined />
-          <span>菜单项1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <video-camera-outlined />
-          <span>菜单项2</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
+  <Flex vertical gap="small">
+    <Flex gap="large" align="center">
+      Use roles as:
+      <Switch
+        :checked="useRolesAsFunction"
+        @change="handleChange"
+        checkedChildren="Function"
+        unCheckedChildren="Object"
+      />
+    </Flex>
 
-    <!-- 主内容区域 -->
-    <a-layout
-      :style="{
-        marginLeft: collapsed ? '0' : '200px',
-        transition: 'margin 0.2s',
-      }"
-    >
-      <a-layout-header style="background: #fff; padding: 0">
-        <menu-unfold-outlined
-          v-if="collapsed"
-          class="trigger"
-          @click="() => (collapsed = !collapsed)"
-        />
-        <menu-fold-outlined
-          v-else
-          class="trigger"
-          @click="() => (collapsed = !collapsed)"
-        />
-      </a-layout-header>
-      <a-layout-content style="margin: 24px 16px">
-        <div
-          :style="{ padding: '24px', background: '#fff', minHeight: '360px' }"
-        >
-          页面内容
-        </div>
-      </a-layout-content>
-    </a-layout>
-  </a-layout>
+    <Flex gap="small" :style="{ alignSelf: 'flex-end' }">
+      <Button @click="addContent"> 新增内容 </Button>
+      <Button
+        @click="
+          () => {
+            listRef?.scrollTo({ key: 0, block: 'nearest' });
+          }
+        "
+      >
+        Scroll To First
+      </Button>
+    </Flex>
+
+    <BubbleList
+      ref="listRef"
+      :style="{ maxHeight: '300px' }"
+      :roles="useRolesAsFunction ? rolesAsFunction : rolesAsObject"
+      :items="charts"
+    />
+  </Flex>
 </template>
+<script setup lang="ts">
+import { UserOutlined } from "@ant-design/icons-vue";
+import { BubbleList } from "ant-design-x-vue";
+import { Button, Flex, Switch } from "ant-design-vue";
+import type { BubbleListProps, BubbleProps } from "ant-design-x-vue";
+import type { SwitchProps } from "ant-design-vue";
+import { ref, h } from "vue";
 
-<script setup>
-import { ref } from "vue";
-import {
-  UserOutlined,
-  VideoCameraOutlined,
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-} from "@ant-design/icons-vue";
+defineOptions({ name: "AXBubbleListSetup" });
 
-const selectedKeys = ref(["1"]);
-const collapsed = ref(false);
+const rolesAsObject: BubbleListProps["roles"] = {
+  ai: {
+    placement: "start",
+    avatar: { icon: h(UserOutlined), style: { background: "#fde3cf" } },
+    typing: { step: 5, interval: 20 },
+    style: {
+      maxWidth: "600px",
+    },
+  },
+  user: {
+    placement: "end",
+    avatar: { icon: h(UserOutlined), style: { background: "#87d068" } },
+  },
+};
+
+const rolesAsFunction = (bubbleData: BubbleProps, index: number) => {
+  const RenderIndex: BubbleProps["messageRender"] = (content) =>
+    h(Flex, null, [h("text", null, `#${index}: ${content}`)]);
+  switch (bubbleData.role) {
+    case "ai":
+      return {
+        placement: "start" as const,
+        avatar: { icon: h(UserOutlined), style: { background: "#fde3cf" } },
+        typing: { step: 5, interval: 20 },
+        style: {
+          maxWidth: 600,
+        },
+        messageRender: RenderIndex,
+      };
+    case "user":
+      return {
+        placement: "end" as const,
+        avatar: { icon: h(UserOutlined), style: { background: "#87d068" } },
+        messageRender: RenderIndex,
+      };
+    default:
+      return { messageRender: RenderIndex };
+  }
+};
+
+const count = ref<number>(3);
+const useRolesAsFunction = ref(false);
+
+// const listRef = useTemplateRef<InstanceType<typeof BubbleList>>(null);
+const listRef = ref<InstanceType<typeof BubbleList>>(null);
+
+const handleChange = (checked: SwitchProps["checked"]) => {
+  useRolesAsFunction.value = checked as boolean;
+};
+
+const charts = ref([
+  { key: 1, role: "ai", content: "12345" },
+  { key: 2, role: "user", content: "999999999" },
+  // {
+  //   key: "ai",
+  //   role: "ai",
+  //   loading: true,
+  // },
+]);
+
+const addContent = () => {
+  console.log("新增内容");
+  charts.value.push({ key: 3, role: "user", content: "allalallalalaaaa", typing: true, });
+};
 </script>
-
-<style scoped>
-.logo {
-  height: 32px;
-  margin: 16px;
-  color: white;
-  text-align: center;
-  line-height: 32px;
-  font-weight: bold;
-}
-
-.trigger {
-  padding: 0 24px;
-  font-size: 18px;
-  line-height: 64px;
-  cursor: pointer;
-  transition: color 0.3s;
-}
-
-.trigger:hover {
-  color: #1890ff;
-}
-
-/* 侧边栏折叠时的动画效果 */
-.custom-sider {
-  transition: all 0.2s;
-}
-
-/* 为内容区域添加过渡效果 */
-.ant-layout {
-  transition: all 0.2s;
-}
-</style>
